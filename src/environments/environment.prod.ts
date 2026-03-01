7935aea5c1c8edadc8a53b32cf4064be2df11fa5
KucheyAashiq <kucchayaashiq@gmail.com>
2026-03-01 15:20:47 +0530

    minor changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  emailjs: {
+    serviceId: 'service_vkbqlnm', // e.g. 'service_abc123'
+    templateId: 'template_bh22yew', // e.g. 'template_xyz789'
+    publicKey: 'ZNGUI8EsZFgjmPnSi', // e.g. 'AbCdEfGhIjKlMnOp'
+  },
+};
